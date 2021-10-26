aeb0e0a7ca89d1d36cea056f0450bf8b749ce4ef
Daniil <92120469+DaniilSHA@users.noreply.github.com>
2021-10-26 20:26:03 +0300

    end commit in project

--- src/FileGeneralInfo.java
+++ src/FileGeneralInfo.java
@@ -34,25 +34,69 @@ public abstract class FileGeneralInfo {
     public abstract String printDetails();
 
     public static void printAll(FileGeneralInfo[] file, int count){
-        String[][] array = new String[count][3];
-        array[0][0] = "File name |";
-        array[0][1] = " Size |";
-        array[0][2] = " Details";
+        String[][] array = new String[count][5];
+        array[0][0] = "File name ";
+        array[0][1] = "|";
+        array[0][2] = " Size ";
+        array[0][3] = "|";
+        array[0][4] = " Details";
+
 
         for (int i = 1; i< array.length; i++) {
-            array[i][0] = "|" + file[i-1].getName();
-            array[i][1] = "|" + file[i-1].getSize();
-            array[i][2] = "|" + file[i-1].printDetails();
+            array[i][0] = file[i-1].getName();
+            array[i][1] = "|";
+            array[i][2] = Integer.toString(file[i-1].getSize());
+            array[i][3] = "|";
+            array[i][4] = file[i-1].printDetails();
         }
 
+        int indexMaxName = array[1][0].length();
+        for (int i = 1; i< array.length; i++) {
+            if (indexMaxName < array[i][0].length()) indexMaxName = array[i][0].length();
+        }
+
+        for (int j=0; j<((indexMaxName/2)-(array[0][0].length())/2); j++)
+            array[0][0] = " " + array[0][0];
+
+        for (int j=array[0][0].length(); j<indexMaxName; j++)
+        array[0][0] += " ";
+
+        for (int i = 1; i< array.length; i++) {
+            if (array[i][0].length() < indexMaxName)
+            {
+                for (int j=array[i][0].length(); j<indexMaxName; j++) {
+                    array[i][0] += " ";
+                }
+            }
+        }
+
+        int indexMaxSize = array[1][2].length();
+        for (int i = 1; i< array.length; i++) {
+            if (indexMaxSize < array[i][2].length()) indexMaxSize = array[i][2].length();
+        }
+
+        for (int i = 1; i< array.length; i++) {
+            if (array[i][2].length() < indexMaxSize)
+            {
+                int lengthOfArraySecoundColumn = array[i][2].length();
+                for (int j=0; j<indexMaxSize-lengthOfArraySecoundColumn; j++) {
+                    array[i][2] = " " + array[i][2];
+                }
+            }
+        }
+
+        for (int j=0; j<((indexMaxSize/2)-(array[0][2].length())/2); j++)
+            array[0][2] = " " + array[0][2];
+
+        for (int j=array[0][2].length(); j<indexMaxSize; j++)
+            array[0][2] += " ";
+
         for (int i = 0; i< array.length; i++) {
-            for (int j=0; j<3; j++) {
+            for (int j=0; j<5; j++) {
                 System.out.print(array[i][j]);
             }
             System.out.println();
         }
-
-
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         FileGeneralInfo[] array = new FileGeneralInfo []{
@@ -9,5 +11,23 @@ public class Main {
 
         FileGeneralInfo.printAll(array, array.length+1);
 
+        System.out.println();
+
+        FileGeneralInfo[] arrayChild = new FileGeneralInfo []{
+                new FileVideo("BackToTheFuture1.avi", "video", 1470984192, "Back to the future I, 1985", 6488, 640, 325),
+                new FileVideo("BackToTheFuture1.avi", "video", 1470984192, "Back to the future I, 1985", 6488, 640, 325),
+                new FileVideo("BackToTheFuture1.avi", "video", 1470984192, "Back to the future I, 1985", 6488, 640, 325),
+                new FileVideo("BackToTheFuture1.avi", "video", 1470984192, "Back to the future I, 1985", 6488, 640, 325),
+                new FileVideo("BackToTheFuture1.avi", "video", 1470984192, "Back to the future I, 1985", 6488, 640, 325),
+                new FileVideo("BackToTheFuture1.avi", "video", 1470984192, "Back to the future I, 1985", 6488, 640, 325),
+        };
+
+        FileGeneralInfo.printAll(arrayChild, arrayChild.length+1);
+
+//        Если я правильно понял, этот метод подходит для массива из объектов дочерних типов, потому что
+//        во всех дочерних класссах, во-первых, всегда содержаться поля "имя файла" и " размер", а, во-вторых,
+//        я реализовал в каждом дочерним классе метод printDetails (с помощью @Override) таким образом, чтобы
+//        он выводил информацию по типу файла согласно задания. Таким образом, я могу использовать данный метод
+//        для любого класса (типа файла) ведь у меня есть реализация на все возможные дочернии классы.
     }
 }
